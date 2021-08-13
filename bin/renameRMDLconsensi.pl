3fe35b2741313475ce5db819e723a566d47b5ff7
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2021-08-13 15:48:38 +0200

    Format comment and add version

--- bin/renameRMDLconsensi.pl
+++ bin/renameRMDLconsensi.pl
@@ -1,23 +1,24 @@
 #! /usr/bin/env perl
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# perl renameRMDLconsensi.pl <input> <prefix> <output>	#
+# perl renameRMDLconsensi.pl <input> <prefix> <output>  #
 # ===================================================== #
-# Replaces sequence names from RepeatModeler standard	#
-# output (consensi.fa and related files) with easy-to-	#
-# use, short names + species identifier as prefix.	#
+# Replaces sequence names from RepeatModeler standard   #
+# output (consensi.fa and related files) with easy-to-  #
+# use, short names + species identifier as prefix.      #
 # ===================================================== #
-# Alexander Suh				    12 Nov 2015 #
+# Alexander Suh                             12 Nov 2015 #
+# v1.0                                                  #
 # ===================================================== #
-# Example:						#
-# "perl renameRMDLconsensi.pl consensi.fa.classified \	#
-# lepSin lepSin_rm1.0.lib"				#
-# -----------------------------------------------------	#
-# Old FASTA header: ">rnd-2_family-111#LINE/CR1-Zenon \	#
-# ( Recon Family Size = 56, Final Multiple Alignment \	#
-# Size = 53 )"						#
-# -----------------------------------------------------	#
-# New FASTA header: ">lepSin2-111#LINE/CR1-Zenon"	#
+# Example:                                              #
+# "perl renameRMDLconsensi.pl consensi.fa.classified \  #
+#     lepSin lepSin_rm1.0.lib"                          #
+# ----------------------------------------------------- #
+# Old FASTA header: ">rnd-2_family-111#LINE/CR1-Zenon \ #
+# ( Recon Family Size = 56, Final Multiple Alignment \  #
+# Size = 53 )"                                          #
+# ----------------------------------------------------- #
+# New FASTA header: ">lepSin2-111#LINE/CR1-Zenon"       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 use strict;
@@ -35,9 +36,9 @@ while (my $line = <IN>) {
     $line =~ s/_family//g;
     if ($line =~ m/^>/) {
         chomp $line;
-	my @parts = split(/ /, $line);
-	my $id = $parts[0];
-	print OUT $id."\n";
+    my @parts = split(/ /, $line);
+    my $id = $parts[0];
+    print OUT $id."\n";
     } else {
         printf OUT $line;
     }
